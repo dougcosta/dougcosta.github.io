d4e4efa276d6b4abb3eda9ec68ead71f7e2f2e3a
Douglas Costa <doug.cost@gmail.com>
2026-09-16 14:24:52 -0300

    Exclusao da constate para o Google Analytics, usando o ID direto no código

--- src/consts.ts
+++ src/consts.ts
@@ -36,4 +36,3 @@ export const BLOG = {
 // Backwards-compatible exports while the site moves from the Astro starter.
 export const SITE_TITLE = SITE.title;
 export const SITE_DESCRIPTION = SITE.description;
-export const GOOGLE_ANALYTICS_ID = 'G-FKJZHRENJ7';

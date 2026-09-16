5c7046cc0e3200a78a7d6f79d628434954c9971d
Douglas Costa <doug.cost@gmail.com>
2026-09-16 13:28:23 -0300

    Inclusao do pluging para que os links dos artigos sejam abertos em outras abas

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,16 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import { satteri } from '@astrojs/markdown-satteri';
+import { externalLinks } from './src/plugins/external-links';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://dougcosta.github.io',
 	integrations: [mdx(), sitemap()],
+	markdown: {
+		processor: satteri({
+			hastPlugins: [externalLinks],
+		}),
+	},
 });
